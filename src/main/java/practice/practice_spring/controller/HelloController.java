0a350f9995f45f58e518ef353e4413ff87b29ed1
ShinHeeYoun <eustaph7725@gmail.com>
2024-09-14 11:10:33 +0900

    토대 만들기

--- src/main/java/practice/practice_spring/controller/HelloController.java
+++ src/main/java/practice/practice_spring/controller/HelloController.java
@@ -11,6 +11,13 @@ public class HelloController {
     @GetMapping("hello")
     public String hello(Model model) {
         model.addAttribute("data", "hello!!!");
-        return "hello";
+        return "hello"; // hello.html을 찾아서 실행시켜라
+        // 컨트롤러에서 리턴 값을 문자를 반환하면 viewResolver가 화면을 찾아서 처리
+        // (resource:templates/ + (ViewName) + .html
+        // 그러면 hello.html 에서 여기 있는 data-hello!!! 값을 가져다 넣음
+        
+        // "hello" url에 매칭이 됨 (http://localhost:8080/hello)
+
+
     }
 }
